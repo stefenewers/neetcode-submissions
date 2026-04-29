e7c66defd5dd8580ff3bead45c6ed3a9ad3e053d
Stefen Ewers <130431737+stefenewers@users.noreply.github.com>
2026-04-29 07:32:28 -0400

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,13 @@
+integer_type = 10
+float_type = 2.5
+bool_type = True
+string_type = "My name is"
+list_type = [1, 2, 3]
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
