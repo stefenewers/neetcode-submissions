d221e69d9a1287dbf8876e4f09597860029d5fd6
Stefen Ewers <130431737+stefenewers@users.noreply.github.com>
2026-04-29 07:34:09 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.1
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "Yes Man"
+print(type(variable))
+variable = [2, 4, 5, 10]
+print(type(variable))
