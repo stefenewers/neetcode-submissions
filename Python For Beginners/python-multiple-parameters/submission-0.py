bbee49bac7990c90c9d0597b3fa7508224b568c3
Stefen Ewers <130431737+stefenewers@users.noreply.github.com>
2026-05-04 10:20:12 -0400

    Add: python-multiple-parameters - submission-0

---
+++ Python For Beginners/python-multiple-parameters/submission-0.py
@@ -0,0 +1,14 @@
+def two_sum(a,b):
+    tewsum = a + b
+    print(tewsum)
+
+def three_sum(c, d, e):
+    treesum = c + d + e
+    print(treesum)
+
+two_sum(7, 10)
+three_sum(3, 5, 8)
+
+# do not modify below this line
+two_sum(10, 9)
+three_sum(5, 14, 6)
