7ea2448da771ec2891e2fcc0e6c977b3753747e6
Stefen Ewers <130431737+stefenewers@users.noreply.github.com>
2026-09-07 18:20:26 -0400

    Add: python-return-statement - submission-1

---
+++ Python For Beginners/python-return-statement/submission-1.py
@@ -0,0 +1,11 @@
+def product(a, b):
+    the_prod = a * b
+    return the_prod
+
+
+
+# don't modify below this line
+print(product(2, 4))
+print(product(8, 2))
+print(product(4, 8))
+print(product(8, 8))
