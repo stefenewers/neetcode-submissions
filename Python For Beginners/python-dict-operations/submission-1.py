2d0a4334d01f13bdc5cbc2aa6bf843c3c463599b
Stefen Ewers <130431737+stefenewers@users.noreply.github.com>
2026-09-10 22:15:42 -0400

    Add: python-dict-operations - submission-1

---
+++ Python For Beginners/python-dict-operations/submission-1.py
@@ -0,0 +1,20 @@
+your_dict = { 
+  "a": 10, 
+  "apple": 12,
+  "bat": 7
+}
+
+#1
+print(your_dict)
+
+#2
+print(your_dict["a"])
+
+#3
+print("d" in your_dict)
+
+#4
+your_dict["a"] = 4
+
+#5
+print(your_dict)
