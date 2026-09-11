d62f71f27c4af7feaefa9832b9347a5a43c0e220
Stefen Ewers <130431737+stefenewers@users.noreply.github.com>
2026-09-11 09:04:14 -0400

    Add: python-type-conversion-input - submission-1

---
+++ Python For Beginners/python-type-conversion-input/submission-1.py
@@ -0,0 +1,18 @@
+def read_integer() -> int:
+    number = int(input(""))
+    return number
+
+
+def read_float() -> float:
+    percentage = float(input(""))
+    return percentage
+
+
+# do not modify below this line
+print(read_integer())
+print(read_integer())
+print(read_integer())
+
+print(read_float())
+print(read_float())
+print(read_float())
