818ab7aa6ab9aec75242ce0d56b80c17401ad358
Stefen Ewers <130431737+stefenewers@users.noreply.github.com>
2026-09-10 23:47:35 -0400

    Add: python-dict-remove - submission-3

---
+++ Python For Beginners/python-dict-remove/submission-3.py
@@ -0,0 +1,11 @@
+from typing import Dict, List
+
+def remove_keys(my_dict: Dict[str, int], keys: List[str]) -> Dict[str, int]:
+    for key in keys:
+        my_dict.pop(key, None)
+    return my_dict
+
+
+# do not modify below this line
+print(remove_keys({"a": 1, "b": 2, "c": 3}, ["a", "c"]))
+print(remove_keys({"a": 1, "b": 2, "c": 3}, ["d"]))
